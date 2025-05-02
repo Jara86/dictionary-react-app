2c9f31693a3c261567873c686d30f69ce6959c40
Jara86 <jarouschka@gmail.com>
2025-05-02 17:27:02 +0200

    add dictionary.js

---
+++ src/Dictionary.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Results from "./Results";
+import "./Dictionary.css";
+
+export default function Dictionary(props) {
+  const [keyword, setKeyword] = useState(props.defaultKeyword);
+  const [results, setResults] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+
+  function handleResponse(response) {
+    setResults(response.data[0]);
+  }
+
+  function search() {
+    // Using the Free Dictionary API
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <h2>What word do you want to look up?</h2>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+              className="form-control search-input"
+            />
+          </form>
+          <div className="hint">
+            suggested words: sunset, wine, yoga, forest...
+          </div>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading...";
+  }
+}
