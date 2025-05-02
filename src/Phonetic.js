6fbcba36db750a29dd5d805552e4a554739d398d
Jara86 <jarouschka@gmail.com>
2025-05-02 17:29:22 +0200

    Create Phonetic.js

---
+++ src/Phonetic.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Phonetic.css";
+
+export default function Phonetic(props) {
+  if (props.phonetic.audio && props.phonetic.text) {
+    return (
+      <div className="Phonetic">
+        <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+          Listen
+        </a>
+        <span className="text">{props.phonetic.text}</span>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
