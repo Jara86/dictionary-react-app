86608d250417f9341a2565e18e9c2ede94907289
Jara86 <jarouschka@gmail.com>
2025-05-02 17:23:23 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Dictionary from "./components/Dictionary";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary defaultKeyword="sunset" />
+        </main>
+        <footer className="text-center mt-5">
+          <small>Coded by Your Name</small>
+        </footer>
+      </div>
     </div>
   );
 }
