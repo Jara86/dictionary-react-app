dd13c4aa52134fd34105d221ea4490e835a928f1
Jara86 <jarouschka@gmail.com>
2025-05-02 17:30:31 +0200

    Create Synonyms.js

---
+++ src/Synonyms.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Synonyms.css";
+
+export default function Synonyms(props) {
+  if (props.synonyms && props.synonyms.length) {
+    return (
+      <div className="Synonyms">
+        <strong>Synonyms: </strong>
+        {props.synonyms.map(function (synonym, index) {
+          return <span key={index}>{synonym}</span>;
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
+
